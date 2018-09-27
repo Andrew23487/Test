7d4d3e997b73964302db508642adfa30552d1bf4
Andrew Lamin <andrewlamin@Andrews-Mac-Pro.local>
2018-09-27 18:01:49 +0100

    Infinite loop set to break after 5 minutes as requested by Jonny

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+import time
+
 x = "This is a Test2"
 
+timeout = time.time() + 60 * 5
+
 while True:
-    print x
+    if time.time() > timeout:   # Infinite loop will break after 5 minutes
+        break
+    else:
+        print x
